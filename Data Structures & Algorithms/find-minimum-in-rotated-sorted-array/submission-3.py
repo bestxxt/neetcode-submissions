f4b7f8beffac5b6d27e520eae7d356fc628cb0a1
Terry Xie <57700744+bestxxt@users.noreply.github.com>
2026-09-04 01:12:31 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if nums[0] <= nums[-1]:
+            return nums[0]
+        
+        # find the jumper
+        lo = 0
+        hi = len(nums) - 1
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == nums[lo]:
+                return nums[mid+1]
+            elif nums[mid] < nums[lo]:
+                hi = mid 
+            elif nums[mid] > nums[lo]:
+                lo = mid 
+        return nums[hi]
